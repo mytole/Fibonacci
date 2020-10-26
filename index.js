3ded8c8fdb5d06b124e977624b081e30dce9dc4f
Matan Toledano <mytole@gmail.com>
2020-10-26 20:08:15 +0200

    2nd_Milestone

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 let fibIn = document.getElementById("num1");
 let ans = document.getElementById("num2");
 
-fibIn.innerText = "4";
-ans.innerText = "3";
+
+
+function fiboCalc(userNum){
+    if(userNum==0){
+        return 0;
+    }
+    if(userNum==1){
+        return 1;
+    }
+
+    
+    let fiboNumPriv2 = 0;
+    let fiboNumPriv1 = 1;
+    let fiboNumCurr = 1;
+
+    for(let fiboIndex = 1;fiboIndex<userNum; fiboIndex++){
+        fiboNumCurr = fiboNumPriv1 + fiboNumPriv2 ;
+        fiboNumPriv2 = fiboNumPriv1 ;
+        fiboNumPriv1 = fiboNumCurr ;
+        
+    }
+
+    return fiboNumCurr;
+}
+
+fibIn.innerText = "10";
+ans.innerText = fiboCalc(fibIn.innerText);
