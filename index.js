e6d655f33505000d72b088bbfbc668a110b17464
Matan Toledano <mytole@gmail.com>
2020-10-28 12:46:15 +0200

    4th_Milestone

--- index.js
+++ index.js
@@ -2,28 +2,11 @@
 let calcBtn = document.getElementById("calcBtn");
 let answer = document.getElementById("answer");
 
-function fiboCalc(userNum) {
-    if (userNum == 0) {
-        return 0;
-    }
-    if (userNum == 1) {
-        return 1;
-    }
-
-    let fiboNumPriv2 = 0;
-    let fiboNumPriv1 = 1;
-    let fiboNumCurr = 1;
-
-    for (let fiboIndex = 1; fiboIndex < userNum; fiboIndex++) {
-        fiboNumCurr = fiboNumPriv1 + fiboNumPriv2;
-        fiboNumPriv2 = fiboNumPriv1;
-        fiboNumPriv1 = fiboNumCurr;
-    }
-    return fiboNumCurr;
-}
-
 calcBtn.addEventListener('click',()=>{
     let userNum = document.getElementById("userNum").value;
-    let fiboCurr = fiboCalc(userNum);
-    answer.innerText = fiboCurr;
-})
+    fetch('http://localhost:5050/fibonacci/'+userNum).then(response => {
+        response.json().then(data =>{
+            answer.innerHTML = data.result;
+        })
+    })
+});
