0d60dce32d82d3c4a6e368699dace41b901adc09
mytole <69907156+mytole@users.noreply.github.com>
2020-10-31 16:55:37 +0200

    6th_Milestone

--- index.js
+++ index.js
@@ -1,48 +1,85 @@
-
 let calcBtn = document.getElementById("calcBtn");
 let answer = document.getElementById("answer");
-let spinner = document.getElementById("loading");
+let spinnerNum = document.getElementById("loading-num");
+let spinnerRes = document.getElementById("loading-res");
 let alert50 = document.getElementById("larger-50-alert");
-function showSpinner() { spinner.style.display = "inline-block"; };
-function hideSpinner() { spinner.style.display = "none"; };
+let resultsRow = document.getElementById("row-results");
+function showSpinner(spinner) { spinner.style.display = "inline-block"; };
+function hideSpinner(spinner) { spinner.style.display = "none"; };
 function hideaAlert() {
     alert50.classList.remove("fade-in");
 }
 function showAlert() {
     alert50.classList.add("fade-in");
 }
+function serverCall() {
+    fetch('http://localhost:5050/getFibonacciResults').then(response => {
+
+        response.json().then(data => {
+            hideSpinner(spinnerRes);
+            let copyArr = [...data.results];
+            let sorted = true;
+            do {
+                sorted = true;
+                for (let i = 0; (i + 1) < copyArr.length; i++) {
+                    if (copyArr[i].createdDate < copyArr[i + 1].createdDate) {
+                        let temp = copyArr[i];
+                        copyArr[i] = copyArr[i + 1];
+                        copyArr[i + 1] = temp;
+                        sorted = false;
+                    }
+                }
+            } while (!sorted)
+            $('.result').remove();
+            for (let i = 0; i < copyArr.length; i++) {
+                let resDate = new Date(copyArr[i].createdDate);
+                let currentCol = document.createElement("div");
+                currentCol.classList.add("col-12", "result");
+                resultsRow.appendChild(currentCol);
+                currentCol.innerHTML = "The Fibonnaci Of <b>" + copyArr[i].number + "</b> is <b>" + copyArr[i].result + "</b>. Calculated at: " + resDate;
+
+            }
+        });
+    })
+
+}
 
 calcBtn.addEventListener('click', () => {
-    let userInput = document.getElementById("userNum").value; 
-    
+    let userInput = document.getElementById("userNum").value;
     answer.id = "answer";
     answer.innerHTML = "";
-    showSpinner();
+    showSpinner(spinnerNum);
+    showSpinner(spinnerRes);
     hideaAlert();
 
     if (userInput > 50) {
-        hideSpinner();
+        hideSpinner(spinnerNum);
         showAlert();
 
     }
     else {
         fetch('http://localhost:5050/fibonacci/' + userInput).then(response => {
             if (!response.ok) {
-                hideSpinner();
-
+                hideSpinner(spinnerNum);
+                hideSpinner(spinnerRes);
                 answer.id = "server-err";
+
                 response.text().then(data => {
                     answer.innerHTML = "Server Error: " + data;
                 });
 
             }
             else {
+
                 response.json().then(data => {
-                    hideSpinner();
+                    hideSpinner(spinnerNum);
                     answer.innerHTML = data.result;
+                    serverCall();
                 })
             }
         })
 
     }
 });
+
+
