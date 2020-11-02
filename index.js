71f660b4fb3a94ae59c56f664b0e32f31acc7bda
Matan Toledano <mytole@gmail.com>
2020-11-02 19:13:38 +0200

    7th_Milestone

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let spinnerNum = document.getElementById("loading-num");
 let spinnerRes = document.getElementById("loading-res");
 let alert50 = document.getElementById("larger-50-alert");
 let resultsRow = document.getElementById("row-results");
+let saveBox = document.getElementById("save");
 function showSpinner(spinner) { spinner.style.display = "inline-block"; };
 function hideSpinner(spinner) { spinner.style.display = "none"; };
 function hideaAlert() {
@@ -12,6 +13,27 @@ function hideaAlert() {
 function showAlert() {
     alert50.classList.add("fade-in");
 }
+
+function fiboCalc(userNum) {
+    if (userNum == 0) {
+        return 0;
+    }
+    if (userNum == 1) {
+        return 1;
+    }
+
+    let fiboNumPriv2 = 0;
+    let fiboNumPriv1 = 1;
+    let fiboNumCurr = 1;
+
+    for (let fiboIndex = 1; fiboIndex < userNum; fiboIndex++) {
+        fiboNumCurr = fiboNumPriv1 + fiboNumPriv2;
+        fiboNumPriv2 = fiboNumPriv1;
+        fiboNumPriv1 = fiboNumCurr;
+    }
+    return fiboNumCurr;
+}
+
 function serverCall() {
     fetch('http://localhost:5050/getFibonacciResults').then(response => {
 
@@ -54,31 +76,38 @@ calcBtn.addEventListener('click', () => {
 
     if (userInput > 50) {
         hideSpinner(spinnerNum);
+        hideSpinner(spinnerRes);
         showAlert();
 
     }
     else {
-        fetch('http://localhost:5050/fibonacci/' + userInput).then(response => {
-            if (!response.ok) {
-                hideSpinner(spinnerNum);
-                hideSpinner(spinnerRes);
-                answer.id = "server-err";
-
-                response.text().then(data => {
-                    answer.innerHTML = "Server Error: " + data;
-                });
-
-            }
-            else {
-
-                response.json().then(data => {
+        if (saveBox.checked) {
+            fetch('http://localhost:5050/fibonacci/' + userInput).then(response => {
+                if (!response.ok) {
                     hideSpinner(spinnerNum);
-                    answer.innerHTML = data.result;
-                    serverCall();
-                })
-            }
-        })
+                    hideSpinner(spinnerRes);
+                    answer.id = "server-err";
 
+                    response.text().then(data => {
+                        answer.innerHTML = "Server Error: " + data;
+                    });
+
+                }
+                else {
+
+                    response.json().then(data => {
+                        hideSpinner(spinnerNum);
+                        answer.innerHTML = data.result;
+                        serverCall();
+                    })
+                }
+            })
+        }
+        else{
+            answer.innerText = fiboCalc(userInput);
+            hideSpinner(spinnerNum);
+            hideSpinner(spinnerRes);
+        }
     }
 });
 
