07f7ffd1fae0381ba4826b1764d6ee4feef6ad73
Matan Toledano <mytole@gmail.com>
2020-11-02 19:26:24 +0200

    7th_Milestone+3.1_Geekout

--- index.js
+++ index.js
@@ -14,26 +14,6 @@ function showAlert() {
     alert50.classList.add("fade-in");
 }
 
-/* function fiboCalc(userNum) {
-    if (userNum == 0) {
-        return 0;
-    }
-    if (userNum == 1) {
-        return 1;
-    }
-
-    let fiboNumPriv2 = 0;
-    let fiboNumPriv1 = 1;
-    let fiboNumCurr = 1;
-
-    for (let fiboIndex = 1; fiboIndex < userNum; fiboIndex++) {
-        fiboNumCurr = fiboNumPriv1 + fiboNumPriv2;
-        fiboNumPriv2 = fiboNumPriv1;
-        fiboNumPriv1 = fiboNumCurr;
-    }
-    return fiboNumCurr;
-} */
-
 function fiboCalc(userNum){
     if(userNum==0){
         return 0;
